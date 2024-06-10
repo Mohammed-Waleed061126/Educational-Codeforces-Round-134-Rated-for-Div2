78c1719d3667fa8764770c76bfc03b32eb88da14
Mohammed Waleed <136664646+Mohammed-Waleed061126@users.noreply.github.com>
2024-06-11 01:57:35 +0300

    Add files via upload

---
+++ A. Image.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <fstream>
+#define ll long long
+#define ld long double
+#define pl pair<ll, ll>
+#define vi vector<ll>
+#define sortx(X) sort(X.begin(),X.end());
+#define el "\n"
+#define yes cout<<"YES"<<el;
+#define no cout<<"NO"<<el;
+#define imp cout<<"-1"<<el;
+#define MW_HY ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ordered_set tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+using namespace __gnu_pbds;
+using namespace std;
+// ----------------------------Functions---------------------------- //
+ll string_to_num(char c, string word){
+  ll val = 0;
+  if (c == 'x')
+  {
+    for (ll i = 0; i < word.size(); i++)
+    {
+      val *= 10;
+      val += word[i]-48;
+    }
+  }
+  else
+  {
+    val += c-48;
+  }
+  return val;
+}
+bool isPrime(ll k)
+{
+  if (k == 1)
+  {
+    return false;
+  }
+  for (ll i = 2; i * i <= k; i++)
+  {
+    if (k % i == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+// ----------------------------------------------------------------- //
+void solution(){
+  string word;
+  set <char> st;
+  for (ll i = 0; i < 2; i++)
+  {
+    cin >> word;
+    st.insert(word[0]);
+    st.insert(word[1]);
+  }
+  if (st.size() == 1)
+  {
+    cout << 0 << el;
+  }
+  if (st.size() == 2)
+  {
+    cout << 1 << el;
+  }
+  if (st.size() == 3)
+  {
+    cout << 2 << el;
+  }
+  if (st.size() == 4)
+  {
+    cout << 3 << el;
+  }
+}
+
+int main(){ MW_HY
+  //freopen("input.txt","r",stdin);
+  //freopen("output.txt","w",stdout);
+  int ntimes  = 1;
+  cin >> ntimes;
+  while (ntimes--)
+  {
+    solution();
+  }
+}
